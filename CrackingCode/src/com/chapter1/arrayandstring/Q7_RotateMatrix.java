b7cde12140082854a96a304e746e2dbf102c82c5
loaner <loaner@SEAS13065.seas.gwu.edu>
2017-05-27 07:23:21 -0400

    save

--- CrackingCode/src/com/chapter1/arrayandstring/Q7_RotateMatrix.java
+++ CrackingCode/src/com/chapter1/arrayandstring/Q7_RotateMatrix.java
@@ -5,65 +5,39 @@ public class Q7_RotateMatrix {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int input[][] = { {1,2,3,4}, {8,7,6,5}, {12,11,10,9}, {16,15,14,13}};
-		int output[][] = rotatematrix(input,4);
+		
 		for(int i = 0; i < 4; i++){
 			for(int j = 0 ; j < 4 ; j++){
 				System.out.print(input[i][j]+" ");
 			}
 			System.out.println("");
 		}
+		rotatematrix(input,4);
 		System.out.println("");
 		for(int i = 0; i < 4; i++){
 			for(int j = 0 ; j < 4 ; j++){
-				System.out.print(output[i][j] + " ");
+				System.out.print(input[i][j] + " ");
 			}
 			System.out.println(" ");
 		}
 	}
 
-	private static int[][] rotatematrix(int[][] input, int n) {
-		int i = 0;
-		int j = 0;
-		int newi = 0;
-		int newj = 0;
-		int output[][] = new int[n][n];
+	private static void rotatematrix(int[][] input, int n) {
+		
 		int limit = n/2;//number of times to perform the operation
 		for(int k = 0; k < limit;k++){
-			
-			//copies the first row
-			for(i = k, j = k, newi = k, newj = n-1-k; j < n-1-k; j++, newi++){
-				output[newi][newj] = input[i][j];
+			int first = k;
+			int last  = n - 1 - k;
+			for(int i = first; i < last; i++){
+				int offset = i - first;
+				int top = input[first][i];
+				input[first][i] = input[last - offset][first];
+				input[last - offset][first] = input[last][last - offset];
+				input[last][last - offset] = input[i][last];
+				input[i][last] = top;
 			}
-			
-			//copies the last column
-			for(i = k, j = n-1-k, newi = n-1-k, newj = n-1-k; i < n-1-k; i++, newj--){
-				output[newi][newj] = input[i][j];
-			}
-			
-			//copies the last row
-			for(i = n-1-k,j = n-1-k, newi = n-1-k, newj = k  ; j >= k; j--, newi--){
-				output[newi][newj] = input[i][j];
-			}
-			
-			//copies the first column
-			for(i = n-1-k, j = k, newi = k, newj = k; i > k; i--, newj++){
-				output[newi][newj] = input[i][j];
-			}
-		
 		}
-		return output;
+		//return output;
 	}
 
 }
-/*for(i = 0, j = 0, newi = 0, newj = n-1; j < n-1; j++, newi++){
-	output[newi][newj] = input[i][j];
-}
-for(i = 0, j = n-1, newi = n-1, newj = n-1; i < n-1; i++, newj--){
-	output[newi][newj] = input[i][j];
-}
-for(i = n-1,j = n-1, newi = n-1, newj = 0  ; j >= 0; j--, newi--){
-	output[newi][newj] = input[i][j];
-}
-for(i = n-1, j = 0, newi = 0, newj = 0; i > 0; i--, newj++){
-	output[newi][newj] = input[i][j];
-}*/
