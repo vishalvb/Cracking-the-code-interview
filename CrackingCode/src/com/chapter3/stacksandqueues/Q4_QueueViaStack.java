06deb6cc757a480230443d46f34c058efcf4376b
loaner <loaner@SEAS13065.seas.gwu.edu>
2017-06-10 17:42:51 -0400

    I had same idea but I just looked at ctci for reference

---
+++ CrackingCode/src/com/chapter3/stacksandqueues/Q4_QueueViaStack.java
@@ -0,0 +1,59 @@
+package com.chapter3.stacksandqueues;
+
+import java.util.Stack;
+
+public class Q4_QueueViaStack<T> {
+
+	Stack<T> stackNewest, stackOldest;
+	public Q4_QueueViaStack(){
+		stackNewest = new Stack<T>();
+		stackOldest = new Stack<T>();
+	}
+	
+	
+	public int size(){
+		return stackNewest.size() + stackOldest.size();
+	}
+	
+	public void add(T value){
+		stackNewest.push(value);
+	}
+	
+	private void shiftStacks(){
+		if(stackOldest.isEmpty()){
+			while(!stackNewest.isEmpty()){
+				stackOldest.push(stackNewest.pop());
+			}
+		}
+	}
+	
+	public T peek(){
+		shiftStacks();
+		return stackOldest.peek();
+	}
+	
+	public T pop(){
+		shiftStacks();
+		return stackOldest.pop();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Q4_QueueViaStack<Integer> stack = new Q4_QueueViaStack<>();
+		stack.add(1);
+		stack.add(2);
+		stack.add(3);
+		stack.add(4);
+		stack.add(5);
+		stack.add(6);
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		stack.add(5);
+		stack.add(6);
+		System.out.println(stack.pop());
+	}
+
+}
